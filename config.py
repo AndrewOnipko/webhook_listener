aaf5c6b689783d63441defadc42d4726ec476279
Andrew <nekoc528@gmail.com>
2025-07-16 20:27:37 +0700

    dev: update path to db in config

--- config.py
+++ config.py
@@ -2,12 +2,12 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev")
-    
+    basedir = os.path.abspath(os.path.dirname(__file__))
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME") 
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD") 
 
-    SQLALCHEMY_DATABASE_URI = "sqlite:///instance/webhook.db"
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'webhook.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
