4eece67aed84736cd525cf80eb234f7e5dd2ca35
Andrew <nekoc528@gmail.com>
2025-07-16 18:05:18 +0700

    dev: add push route

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -1,10 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 from app.services.email_service import EmailService
+from app.handlers.push_event_handler import PushEventHandler
+from app.logger import simple_logger
 
 webhook_bp = Blueprint("webhook", __name__)
 
+@simple_logger
 @webhook_bp.route("/test-email")
 def send_test_email():
     email_service = EmailService()
     email_service.send_email("Тест", "Это тестовое письмо", "nekoc528@gmail.com")
     return "Письмо отправлено"
+
+@simple_logger
+@webhook_bp.route("/webhook", methods=["POST"])
+def push_webhook():
+    event_type = request.headers.get("X-GitHub-Event")
+
+    if event_type == 'push':
+        payload = request.get_json()
+        email_service = EmailService()
+        handler = PushEventHandler(email_service)
+        handler.handle(payload)
+
+    return jsonify({"status": "ok"})
